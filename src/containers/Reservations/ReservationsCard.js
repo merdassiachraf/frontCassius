35ab964d0563b0ad886c9ace4d44f24bda66f707
merdassiachraf <achrafmerdassi@outlook.com>
2020-05-17 05:06:39 +0200

    5:06 17/5/2020

--- src/containers/Reservations/ReservationsCard.js
+++ src/containers/Reservations/ReservationsCard.js
@@ -1,14 +1,21 @@
-import React from 'react'
+import React from "react";
+import { MDBIcon } from "mdbreact";
 
 export default function ReservationsCard() {
-    return (
-        <div className="card-reservation p-4 m-4 align-items-center">
-           <h5>N°12536985474</h5>
-           <div className="d-flex">
-           <h5>Car : </h5> <h5>Toyota Yaris</h5></div>
-           <h5>Client Name: Merdassi Achraf</h5>
-           <h5>Total Price : 5258 dt</h5> 
-           <h5>Total of Days : 32 days</h5> 
-        </div>
-    )
+  return (
+    <div className="card-reservation p-4 m-4 align-items-center">
+      <div className="d-flex justify-content-between">
+        <h5>N°12536985474</h5>
+        {/* <MDBIcon className="green-text" size="lg" icon="check" /> */}
+        {/* <MDBIcon className="text-danger" size="lg" icon="times" /> */}
+        <MDBIcon className="text-warning" size="lg" icon="user-edit" />
+      </div>
+      <div className="d-flex">
+        <h5>Car : </h5> <h5>Toyota Yaris</h5>
+      </div>
+      <h5>Client Name: Merdassi Achraf</h5>
+      <h5>Total Price : 5258 dt</h5>
+      <h5>Total of Days : 32 days</h5>
+    </div>
+  );
 }
